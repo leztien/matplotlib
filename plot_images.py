0cc026c5e1848f719a2ca245aeb50ce8e785a307
I.N <48551497+leztien@users.noreply.github.com>
2020-05-16 18:02:43 +0200

    Update plot_images.py

--- plot_images.py
+++ plot_images.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 
 
-def plot_digits(height, width, data, target=None, image_shape=None):
+def plot_digits(height, width, data, target=None, image_shape=None, title=None):
     image_shape = image_shape or ([int(len(data[0])**.5),]*2 if len(data[0].shape)==1 else data[0].shape)
     fig, axs = plt.subplots(nrows=height, ncols=width, figsize=(width,height),
                             subplot_kw={'xticks':[], 'yticks':[]},
                             gridspec_kw=dict(hspace=0.1, wspace=0.1))
     for (i,sp) in enumerate(axs.flat):
         sp.imshow(data[i].reshape(*image_shape), cmap='binary', interpolation='nearest', clim=(0,16))
-        sp.text(0.85, 0.05, str(y[i]), transform=sp.transAxes, color='navy')
-        if target is None: sp.axis("off")
+        if target is None:
+            sp.axis("off")
+        else:
+            sp.text(0.85, 0.05, str(y[i]), transform=sp.transAxes, color='navy')
     return(fig,axs)
 
 ##############################################################################
@@ -19,4 +21,17 @@ d = load_digits()
 X,y = d.data, d.target
 pn = [x.reshape(8,8) for x in X]
 
-plot_digits(height=5, width=10, data=X, target=y)
+#add noise to digits
+import numpy as np
+X = np.random.normal(loc=X, scale=3)
+
+plot_digits(height=5, width=10, data=X)
+
+#PCA
+from sklearn.decomposition import PCA
+pca = PCA(0.50).fit(X)
+print ("number of components", pca.n_components_)
+
+Xpca = pca.transform(X)
+Xfiltered = pca.inverse_transform(Xpca)
+plot_digits(height=5, width=10, data=Xfiltered, title="dfdf")
